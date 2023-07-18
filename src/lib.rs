61764245cfe98bed6f134e6bff1858a0728efc10
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-07-18 10:32:36 -0400

    commit before matryoshka

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,21 @@ pub mod range {
     #[derive(Debug)]
     pub enum Error {
         AllocationFailed,
+        DriverError(cudarc::driver::result::DriverError),
+        LayoutError(core::alloc::LayoutError),
+        KernelNotFound,
+        LengthMismatch,
+    }
+}
+
+impl From<cudarc::driver::result::DriverError> for range::Error {
+    fn from(error: cudarc::driver::result::DriverError) -> Self {
+        range::Error::DriverError(error)
+    }
+}
+
+impl From<core::alloc::LayoutError> for range::Error {
+    fn from(error: core::alloc::LayoutError) -> Self {
+        range::Error::LayoutError(error)
     }
 }
