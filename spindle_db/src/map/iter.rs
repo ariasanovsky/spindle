47f216d2e07bc0690a41be9c318cf550cdf63515
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-08-08 07:11:17 -0400

    patchy Vec iter instead of lazy iter

--- spindle_db/src/map/iter.rs
+++ spindle_db/src/map/iter.rs
@@ -3,29 +3,37 @@ use crate::{TypeDb, DbResult};
 use super::DbMap;
 
 // iter over all DbMaps in the db
-impl<'a> TypeDb {
+impl TypeDb {
     // fallible function to iterate over all DbMaps
-    pub fn map_iter(&'a self) -> DbResult<MapIter<'a>> {
-        let stmt = self.conn.prepare("SELECT uuid FROM maps")?;
-        Ok(MapIter { db: self, stmt })
+    pub fn map_iter(&self) -> DbResult<MapIter /* <'a> */ > {
+        let mut stmt = self.conn.prepare("SELECT uuid FROM maps")?;
+        let maps: Vec<DbMap> = stmt
+            .query_map([], |row| {
+                let uuid: String = row.get(0)?;
+                self.get_map_from_uuid(uuid)
+            })?
+            .collect::<DbResult<_>>()?;
+        Ok(MapIter { maps })
     }
 }
 
 // iterator over all DbMaps in the db
-pub struct MapIter<'a> {
-    db: &'a TypeDb,
-    stmt: rusqlite::Statement<'a>,
+// https://docs.rs/rusqlite/latest/rusqlite/struct.MappedRows.html#method.into_iter
+// lazily we'll just collect a Vec when the iterator is created
+// todo! make this lazy
+// pub struct MapIter<'a> {
+//     db: &'a TypeDb,
+//     stmt: rusqlite::Statement<'a>,
+// }
+pub struct MapIter {
+    maps: Vec<DbMap>,
 }
 
-impl<'a> Iterator for MapIter<'a> {
-    type Item = DbResult<DbMap>;
+// impl<'a> Iterator for MapIter<'a> {
+impl Iterator for MapIter {
+    type Item = /* DbResult< */ DbMap /* > */;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let uuid: String = match self.stmt.query_row([], |row| row.get(0)) {
-            Ok(uuid) => uuid,
-            Err(rusqlite::Error::QueryReturnedNoRows) => return None,
-            Err(e) => return Some(Err(e.into())),
-        };
-        Some(self.db.get_map_from_uuid(uuid))
+        self.maps.pop()
     }
 }
