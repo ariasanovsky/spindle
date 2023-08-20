929f1d00ce01c802248109eaf9e094a5288e3596
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-08-20 05:05:35 -0400

    setting explicit goal for range fn init

--- examples/02-range.rs
+++ examples/02-range.rs
@@ -1,16 +1,14 @@
 // use spindle::{DevSlice, HostSlice};
-// use spindle::U64; // pub type U64 = u64;
-// use spindle::Any; // pub struct Any;
-// #[spindle::map(#example_02)]
-// fn square_over_two(x: Any, i: U64) -> u64 {
-//     (x * x) / 2
+// #[spindle::init(#example_02)]
+// fn square_over_two(x: u64) -> f32 {
+//     ((x * x) / 2) as f32
 // }
 
 // spindle::spin!(#example_02, U = u64);
 
 fn main() -> spindle::Result<()> {
     todo!("implement range maps");
-    // let nums: DevSlice<U, u64> = (0..100_000).square_over_two()?;
+    // let nums: DevSlice<U, u64> = (100_000..200_000).square_over_two()?;
     // let nums: HostSlice<U, u64> = nums.try_to_host()?;
     // nums.iter().enumerate().for_each(|(i, x)| {
     //     assert_eq!(x, square_over_two(i as u64));
